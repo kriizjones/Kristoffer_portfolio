a52bdd914258dbb88524aa3418b348128600fd17
kriizjones <kristoffer.s.karlsson@hotmail.com>
2016-12-06 10:21:20 +0100

    have changed files to haml

---
+++ spec/features/index_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe 'index', type: :feature do
+
+  before do
+    visit '/'
+  end
+end
+
+# it 'displays project list' do
+#     expect(page).to have_css '.projects'
+#     within '.projects' do
+#       expect(page).to have_content 'My First Website'
+#       expect(page).to have_content 'FizzBuzz'
+#     end
+#   end
+#
+# it 'renders footer partial' do
+#   expect(page).to have_selector 'footer'
+#   within 'footer' do
+#     expect(page).to have_content 'My Portfolio'
+#     expect(page).to have_content 'Built using the awesome Middleman framework'
+#   end
+# end
